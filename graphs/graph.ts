0061d579067c03039ce281ba13a014c3a7709e84
Maheen <Maheen@zenikas-mbp.lan>
2019-11-12 21:33:32 +0800

    Feat : Remove vertex

--- graphs/graph.ts
+++ graphs/graph.ts
@@ -4,9 +4,9 @@ class Graph {
     this.adjacentList = {};
   }
 
-  addVertex(vertex) {
-    if (!this.adjacentList[vertex]) {
-      this.adjacentList[vertex] = [];
+  addVertex(vtx) {
+    if (!this.adjacentList[vtx]) {
+      this.adjacentList[vtx] = [];
     }
   }
 
@@ -14,9 +14,18 @@ class Graph {
     this.adjacentList[v1].push(v2);
     this.adjacentList[v2].push(v1);
   }
+
   removeEdge(v1, v2) {
     this.adjacentList[v1] = this.adjacentList[v1].filter(v => v !== v2);
     this.adjacentList[v2] = this.adjacentList[v2].filter(v => v !== v1);
+  }
+
+  removeVertex(v1) {
+    while (this.adjacentList[v1].length > 0) {
+      const edge = this.adjacentList[v1].shift();
+      this.removeEdge(v1, edge);
+    }
+    delete this.adjacentList[v1];
   }
 }
 
@@ -24,7 +33,9 @@ const graph = new Graph();
 graph.addVertex("Tokyo");
 graph.addVertex("New York");
 graph.addVertex("London");
+graph.addEdge("London", "New York");
 graph.addEdge("Tokyo", "London");
 graph.addEdge("Tokyo", "New York");
 graph.removeEdge("Tokyo", "New York");
+graph.removeVertex("Tokyo");
 console.log("====>", graph.adjacentList);
